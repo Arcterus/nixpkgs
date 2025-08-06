3dda58f1bc9df33f109a417507f2e0988b27c50e
Alex Lyon <dev@arct.rs>
2025-08-05 23:50:11 -0700

    try resetting manual config

--- pkgs/os-specific/linux/kernel/manual-config.nix
+++ pkgs/os-specific/linux/kernel/manual-config.nix
@@ -306,12 +306,10 @@ lib.makeOverridable (
 
           # reads the existing .config file and prompts the user for options in
           # the current kernel source that are not found in the file.
-          # make "''${makeFlagsArray[@]}"" oldconfig
-          make ${configfile.makeFlags} oldconfig
+          make $makeFlags "''${makeFlagsArray[@]}" oldconfig
           runHook postConfigure
 
-          # make "''${makeFlagsArray[@]}" prepare
-          make ${configfile.makeFlags} prepare
+          make $makeFlags "''${makeFlagsArray[@]}" prepare
           actualModDirVersion="$(cat $buildRoot/include/config/kernel.release)"
           if [ "$actualModDirVersion" != "${modDirVersion}" ]; then
             echo "Error: modDirVersion ${modDirVersion} specified in the Nix expression is wrong, it should be: $actualModDirVersion"
@@ -437,8 +435,7 @@ lib.makeOverridable (
           if [ -z "''${dontStrip-}" ]; then
             installFlagsArray+=("INSTALL_MOD_STRIP=1")
           fi
-          # make modules_install "''${makeFlagsArray[@]}" \
-          make modules_install ${configfile.makeFlags} \
+          make modules_install $makeFlags "''${makeFlagsArray[@]}" \
             $installFlags "''${installFlagsArray[@]}"
           unlink $out/lib/modules/${modDirVersion}/build
           rm -f $out/lib/modules/${modDirVersion}/source
@@ -453,8 +450,7 @@ lib.makeOverridable (
           cd $dev/lib/modules/${modDirVersion}/source
 
           cp $buildRoot/{.config,Module.symvers} $dev/lib/modules/${modDirVersion}/build
-          # make modules_prepare "''${makeFlagsArray[@]}" O=$dev/lib/modules/${modDirVersion}/build
-          make modules_prepare ${configfile.makeFlags} O=$dev/lib/modules/${modDirVersion}/build
+          make modules_prepare $makeFlags "''${makeFlagsArray[@]}" O=$dev/lib/modules/${modDirVersion}/build
 
           # For reproducibility, removes accidental leftovers from a `cc1` call
           # from a `try-run` call from the Makefile
